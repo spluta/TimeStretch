0da01a6da104f4b3a2e8be6945bb22106d783b9a
Sam <spluta@gmail.com>
2021-07-31 21:28:48 +0200

    fixed 88200+ and write by chunks

---
+++ rust/target/rls/debug/deps/ness_stretch-c84813e144b1380e.d
@@ -0,0 +1,5 @@
+/Users/spluta1/Library/Application Support/SuperCollider/Extensions/TimeStretch/rust/target/rls/debug/deps/ness_stretch-c84813e144b1380e.rmeta: src/main.rs
+
+/Users/spluta1/Library/Application Support/SuperCollider/Extensions/TimeStretch/rust/target/rls/debug/deps/ness_stretch-c84813e144b1380e.d: src/main.rs
+
+src/main.rs:
